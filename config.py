09f1d98a23627d357dbe6c2e86250a60c17d882c
Hartomeister <64258322+Hartomeister@users.noreply.github.com>
2022-06-15 19:09:37 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from aiogram import Bot
+
+API_TOKEN = '5469766786:AAF8Ba-LhMnk--Kf0R5IA-hS7M9jMQBmOjA' # Токен бота, получать у @BotFather
+proxy_url = 'http://proxy.server:3128'
+TOKEN = Bot(token=API_TOKEN, proxy=proxy_url)
+QIWI_ACCOUNT = 'https://qiwi.com/n/MADVLD444' # Номер телефона киви кошелька (71111111111)
+BTC_ADDRESS = 'Временно недоступно' # Номер btc адреса на которую будут поступать средства
+CARD_NUMBER = 'https://qiwi.com/n/MADVLD444' # Номер банк. карты на которую будут поступать средства
